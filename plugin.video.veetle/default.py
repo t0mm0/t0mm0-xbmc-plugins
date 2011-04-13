574c231ea869f11238444778a0f411c39dd17e35
t0mm0 <dev@onairsoftware.co.uk>
2011-04-13 23:55:41 +0100

    add ability to grab and display schedule data.
    data seems to be patchy and not always useful but is good on some channels. as it take a few secs to scrape this can be turned off in the plugin settings.

--- plugin.video.veetle/default.py
+++ plugin.video.veetle/default.py
@@ -16,13 +16,16 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import xbmc, xbmcplugin, xbmcgui
+import os
 import simplejson as json
+import time
 import urllib, urllib2
+import xbmc, xbmcaddon, xbmcplugin, xbmcgui
 
 pluginUrl = sys.argv[0]
 pluginHandle = int(sys.argv[1])
 pluginQuery = sys.argv[2]
+__language__ = xbmcaddon.Addon(id='plugin.video.veetle').getLocalizedString
 
 BASE_URL = 'http://www.veetle.com'
 CHANNEL_LISTING = BASE_URL + '/channel-listing-cross-site.js'
@@ -38,29 +41,73 @@ def get_ajax_json(url):
         return False
 
 def get_stream_url(channel_id):
-    return get_ajax_json(BASE_URL + '/index.php/channel/ajaxStreamLocation/' + channel_id + '/flash')
+    return get_ajax_json(BASE_URL + '/index.php/channel/ajaxStreamLocation/' + 
+                         channel_id + '/flash')
 
 def get_channel_info(channel_id):
     try:
-        return json.loads(get_ajax_json(BASE_URL + '/index.php/channel/ajaxInfo/' + channel_id))
+        return json.loads(get_ajax_json(BASE_URL + 
+                                        '/index.php/channel/ajaxInfo/' + 
+                                        channel_id))
     except ValueError, error_info:
         print 'Problem getting channel info: ' + channel_id
         print 'Error returned: ', error_info
         return {'title': 'unknown',
                 'description': 'not available',
-                'logo': {'sm': '', 'lg': ''}} 
+                'logo': {'sm': '', 'lg': ''},
+                'programme': {'success': False}
+                } 
+
+class VeetleSchedule():
+    def __init__(self, json_playlist, start_time, reference_clock):
+        self.start_time = start_time
+        self.playlist = self.populate_playlist(json_playlist, reference_clock)
+                
+    def get_now_playing(self):
+        now = time.time()
+        for item in self.playlist:
+            if now >= item['start'] and now < item['end']:
+                return time.strftime('(%H:%M) ', 
+                                     time.localtime(item['start'])
+                                    ) + item['title']
+        return '' 
+        
+    def get_schedule(self):
+        now = time.time()
+        schedule = ''
+        for item in self.playlist:
+            if now < item['end']: 
+                schedule = schedule + time.strftime('(%H:%M) ', 
+                           time.localtime(item['start'])) + item['title'] + '\n'
+        return schedule
+        
+    def populate_playlist(self, json_playlist, reference_clock):
+        playlist = []
+        now = time.time()
+        t = self.start_time - (now - reference_clock)
+        for item in sorted(json_playlist, key=lambda i: i['playOrder']):
+            playlist.append({'title': item['title'],
+                             'description': item['description'],
+                             'duration': item['durationInSeconds'],
+                             'start': t,
+                             'end': t + item['durationInSeconds']})
+            t = t + item['durationInSeconds']
+        return playlist
+
 
 if pluginQuery.startswith('?play='):
     #Play a stream with the given channel id
     channel_id = pluginQuery[6:]
     print 'veetle.com Channel ID: ' + channel_id
     stream_url = get_stream_url(channel_id)
-    if stream_url:
-        channel_info = get_channel_info(channel_id)            
-        listitem = xbmcgui.ListItem(channel_info['title'], iconImage=channel_info['logo']['sm'], thumbnailImage=channel_info['logo']['sm'])
-        infoLabels = {'plot': channel_info['description']}
-        listitem.setInfo('video', infoLabels)
-        xbmc.Player().play(stream_url, listitem)    
+    if stream_url:        
+        xbmcplugin.setResolvedUrl(pluginHandle, True, 
+                                  xbmcgui.ListItem(path=stream_url))
+    else:
+        xbmcplugin.setResolvedUrl(pluginHandle, False, 
+                                  xbmcgui.ListItem())
+        dialog = xbmcgui.Dialog()
+        ok = dialog.ok(__language__(30000), __language__(30001))   
 else:
     #Load the channel list
     response = urllib2.urlopen(CHANNEL_LISTING)
@@ -69,10 +116,12 @@ else:
     print 'Total veetle.com Channels: %d' % len(channels)
 
     #only list channels we can stream
-    channels = [channel for channel in channels if channel.get('flashEnabled', False)]
-
+    channels = [channel for channel in channels if channel.get('flashEnabled', 
+                                                               False)]
+                                                               
     print 'Flash Enabled veetle.com Channels: %d' % len(channels)
 
+    do_grab = xbmcplugin.getSetting(pluginHandle, 'grab_schedule')
     for channel in channels:
         url = pluginUrl + '?play=' + channel['channelId']
         sm = channel['logo'].get('sm', '')
@@ -81,10 +130,22 @@ else:
             thumb = lg
         else:
             thumb = sm
+
+        if do_grab == 'true':
+            channel_info = get_channel_info(channel['channelId'])            
+            if channel_info['programme']['success']:
+                schedule = VeetleSchedule(channel_info['programme']['payload'], 
+                                          channel_info['broadcastStartedTime'],
+                                          channel_info['referenceClock'])
+                channel['title'] += ' ' + schedule.get_now_playing()
+                channel['description'] += '\n' + schedule.get_schedule()
         
-        listitem = xbmcgui.ListItem(channel['title'], iconImage=thumb, thumbnailImage=thumb)
+        listitem = xbmcgui.ListItem(channel['title'], iconImage=thumb, 
+                                    thumbnailImage=thumb)
         infoLabels = {'plot': channel['description']}
         listitem.setInfo('video', infoLabels)
-        xbmcplugin.addDirectoryItem(pluginHandle, url, listitem, isFolder=False)
+        listitem.setProperty('IsPlayable', 'true')
+        xbmcplugin.addDirectoryItem(pluginHandle, url, listitem, 
+                                    isFolder=False, totalItems=len(channels))
     xbmcplugin.endOfDirectory(pluginHandle)
 
